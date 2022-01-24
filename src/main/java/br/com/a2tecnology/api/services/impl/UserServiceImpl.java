0dc22d92d3441723d46131aeebd60d98bdbadbe9
RodrigoNunesGalvan <rodrigongalvan@hotmail.com>
2022-01-24 20:18:30 -0300

    Create Class UserServiceImpl

---
+++ src/main/java/br/com/a2tecnology/api/services/impl/UserServiceImpl.java
@@ -0,0 +1,22 @@
+package br.com.a2tecnology.api.services.impl;
+
+import br.com.a2tecnology.api.domain.User;
+import br.com.a2tecnology.api.repositories.UserRepository;
+import br.com.a2tecnology.api.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    @Autowired
+    private UserRepository repository;
+
+    @Override
+    public User findById(Integer id) {
+        Optional<User> obj = repository.findById(id);
+        return obj.orElse(null);
+    }
+}
