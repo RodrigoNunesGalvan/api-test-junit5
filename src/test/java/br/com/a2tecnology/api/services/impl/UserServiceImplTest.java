fe7fa5d0eb0f771969c469e2ada0005b15bd476d
RodrigoNunesGalvan <rodrigongalvan@hotmail.com>
2022-01-26 17:53:41 -0300

    test for UserService class findById method Object Not Found

--- src/test/java/br/com/a2tecnology/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/a2tecnology/api/services/impl/UserServiceImplTest.java
@@ -3,6 +3,7 @@ package br.com.a2tecnology.api.services.impl;
 import br.com.a2tecnology.api.domain.User;
 import br.com.a2tecnology.api.domain.dto.UserDto;
 import br.com.a2tecnology.api.repositories.UserRepository;
+import br.com.a2tecnology.api.services.exceptions.ObjectNotFoundException;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -59,6 +60,18 @@ class UserServiceImplTest {
         assertEquals(ID, response.getId());
         assertEquals(NAME, response.getName());
         assertEquals(EMAIL, response.getEmail());
+    }
+
+    @Test
+    void whenFindByIdThenReturnAnObjectNotFoundException() {
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException("Objeto não encontrado"));
+
+        try {
+            service.findById(ID);
+        } catch (Exception ex) {
+            assertEquals(ObjectNotFoundException.class, ex.getClass());
+            assertEquals("Objeto não encontrado", ex.getMessage());
+        }
     }
 
     @Test
