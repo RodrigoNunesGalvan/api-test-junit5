4d02d9bc57ae69260edaa12cb705fee0529bafac
RodrigoNunesGalvan <rodrigongalvan@hotmail.com>
2022-01-26 17:56:49 -0300

    introduce constant

--- src/test/java/br/com/a2tecnology/api/services/impl/UserServiceImplTest.java
+++ src/test/java/br/com/a2tecnology/api/services/impl/UserServiceImplTest.java
@@ -30,6 +30,7 @@ class UserServiceImplTest {
     public static final String NAME     = "Rodrigo";
     public static final String EMAIL    = "rodrigo@email.com";
     public static final String PASSWORD = "123";
+    public static final String OBJETO_NAO_ENCONTRADO = "Objeto não encontrado";
     @InjectMocks
     private UserServiceImpl service;
 
@@ -64,13 +65,13 @@ class UserServiceImplTest {
 
     @Test
     void whenFindByIdThenReturnAnObjectNotFoundException() {
-        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException("Objeto não encontrado"));
+        when(repository.findById(anyInt())).thenThrow(new ObjectNotFoundException(OBJETO_NAO_ENCONTRADO));
 
         try {
             service.findById(ID);
         } catch (Exception ex) {
             assertEquals(ObjectNotFoundException.class, ex.getClass());
-            assertEquals("Objeto não encontrado", ex.getMessage());
+            assertEquals(OBJETO_NAO_ENCONTRADO, ex.getMessage());
         }
     }
 
