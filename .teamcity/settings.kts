93c916c367d32ec3d99c4256ff015255211dbe8d
RodrigoNunesGalvan <rodrigongalvan@hotmail.com>
2022-06-11 16:00:14 -0300

    Test Kotlin TeamCity

--- .teamcity/settings.kts
+++ .teamcity/settings.kts
@@ -40,8 +40,7 @@ object Build : BuildType({
 
     steps {
         maven {
-            val myMavenGoal = "clean install"
-            goals = myMavenGoal
+            goals = "clean install"
             runnerArgs = "-Dmaven.test.failure.ignore=true"
         }
     }
